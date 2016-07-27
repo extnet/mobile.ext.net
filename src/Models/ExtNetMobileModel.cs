646d892dc8f25f992f3aae23818a2694963560cf
DaniilVeriga <daniil@object.net>
2016-07-27 18:30:49 +0300

    ExtNetMobileModel.cs has been removed since it is not used anywhere.

--- src/Models/ExtNetMobileModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Ext.Net.Mobile.Examples.Models
-{
-    public class ExtNetMobileModel
-    {
-        public string Title { get; set; }
-        public string TextAreaEmptyText { get; set; }
-    }
-}
